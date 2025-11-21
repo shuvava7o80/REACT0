f3a9c330a605ca1bbe3fe45170bde65731094d90
Cybersport <Cybersport@26kadr.internal>
2025-11-21 16:15:05 +0300

    Version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+//import { useState } from 'react'
+
+import HeaderComponent from './components/Header';
+
+import './index.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <HeaderComponent />
+      </div>
+    </>
+  )
+}
+
+export default App
